8f4a977b774ced24518412051b5c19e3d0e5bd1b
9t8 <63334933+9t8@users.noreply.github.com>
2023-06-04 02:05:26 -0700

    backup

--- components/AlumList.tsx
+++ components/AlumList.tsx
@@ -1,28 +1,51 @@
 "use client";
 
-import { useNhostClient } from "@nhost/nextjs";
-import { useEffect, useState } from "react";
+import { gql, useQuery } from "@apollo/client";
+import { useMemo } from "react";
 
-const ALUMS = `{
-  people(where: {grad_year: {_is_null: false}}, order_by: {grad_year: desc}) {
-    first_name
-    last_name
-    grad_year
+const QUERY = gql`
+  {
+    people(
+      where: { grad_year: { _is_null: false } }
+      order_by: [{ last_name: asc }, { first_name: asc }]
+    ) {
+      first_name
+      last_name
+      grad_year
+    }
   }
-}`;
+`;
 
 export default function AlumList() {
-  const nhost = useNhostClient();
+  const { data } = useQuery(QUERY);
+  const alums = useMemo(
+    () =>
+      data &&
+      data.people.reduce((acc: any[], alum: any) => {
+        acc[alum.grad_year] = [...(acc[alum.grad_year] || []), alum];
+        return acc;
+      }, []),
+    [data]
+  );
 
-  const [alums, setAlums] = useState<any>();
+  console.log(alums);
 
-  useEffect(() => {
-    (async () => {
-      setAlums(await nhost.graphql.request(ALUMS));
-    })();
-  }, [nhost.graphql]);
-
-  console.log(alums?.data.people);
-
-  return <p>Done!</p>;
+  return alums ? (
+    alums.reduceRight(
+      (acc: any[], year: any[], i: number) => [
+        ...acc,
+        <details key={i}>
+          <summary>{i}</summary>
+          <ul>
+            {year.map((person: any, i: number) => (
+              <li key={i}>{person.first_name + " " + person.last_name}</li>
+            ))}
+          </ul>
+        </details>,
+      ],
+      []
+    )
+  ) : (
+    <progress />
+  );
 }
