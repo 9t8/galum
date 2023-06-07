fa20eae570bd727ffa9598858db6be9615fcafba
9t8 <63334933+9t8@users.noreply.github.com>
2023-06-06 23:22:01 -0700

    link alums to profiles

--- components/AlumList.tsx
+++ components/AlumList.tsx
@@ -9,8 +9,20 @@ export default function AlumList() {
   const { data } = useQuery(
     graphql(`
       query selectAlums {
-        people(
-          where: { grad_year: { _is_null: false } }
+        account: people(
+          where: {
+            grad_year: { _is_null: false }
+            user_id: { _is_null: false }
+          }
+          order_by: [{ last_name: asc }, { first_name: asc }, { user_id: asc }]
+        ) {
+          first_name
+          last_name
+          grad_year
+          user_id
+        }
+        no_account: people(
+          where: { grad_year: { _is_null: false }, user_id: { _is_null: true } }
           order_by: [{ last_name: asc }, { first_name: asc }]
         ) {
           first_name
@@ -21,26 +33,34 @@ export default function AlumList() {
     `)
   );
 
-  const alums = useMemo(
+  const groupedByYear = useMemo(
     () =>
       data &&
-      data.people.reduce((acc, alum) => {
-        acc[alum.grad_year!] = [...(acc[alum.grad_year!] || []), alum];
+      [...data.account, ...data.no_account].reduce((acc, e) => {
+        acc[e.grad_year!] = [...(acc[e.grad_year!] || []), e];
         return acc;
-      }, [] as (typeof data.people)[number][][]),
+      }, [] as (typeof data.account)[number][][]),
     [data]
   );
 
-  return alums ? (
+  return groupedByYear ? (
     <>
-      {alums.reduceRight(
+      {groupedByYear.reduceRight(
         (acc, year, i) => [
           ...acc,
           <details key={i}>
             <summary>{i}</summary>
             <ul>
               {year.map((person, i) => (
-                <li key={i}>{person.first_name + " " + person.last_name}</li>
+                <li key={i}>
+                  {person.user_id ? (
+                    <a href={`profile?id=${person.user_id}`}>
+                      {person.first_name + " " + person.last_name}
+                    </a>
+                  ) : (
+                    person.first_name + " " + person.last_name
+                  )}
+                </li>
               ))}
             </ul>
           </details>,
