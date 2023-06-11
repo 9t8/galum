745a5136238bf885ce893e3a293237d46bb37595
9t8 <63334933+9t8@users.noreply.github.com>
2023-06-10 22:24:51 -0700

    ugh

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 There is no error handling.
 
 I do not properly manage the Apollo cache since bad things (inconsistent errors) happen when I try.
+
+I am excited to never work with Nhost again.
+
+GraphQL needs more curly braces.
--- components/Connect.tsx
+++ components/Connect.tsx
@@ -1,12 +1,43 @@
 "use client";
 
-import { useUserId } from "@nhost/nextjs";
-import { useState } from "react";
+import { SignedIn, SignedOut, useUserId } from "@nhost/nextjs";
+import { useEffect, useState } from "react";
 
 export default function Connect() {
   const id = useUserId();
 
+  const [loading, setLoading] = useState(true);
   const [email, setEmail] = useState("");
 
-  return <></>;
+  useEffect(() => setLoading(!id), [id]);
+
+  const handleSubmit = async () => {
+    setLoading(true);
+    const a = await fetch(
+      `${process.env.NEXT_PUBLIC_NHOST_FUNCTIONS_URL}/send`,
+      { method: "POST" }
+    );
+    console.log(await a.text());
+    setLoading(false);
+  };
+
+  return (
+    <>
+      <SignedIn>
+        <input
+          type="text"
+          placeholder="Pausd Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <button onClick={handleSubmit} disabled={loading}>
+          Send Email
+        </button>
+      </SignedIn>
+      <SignedOut>
+        <p>Sign in or sign up to connect your account.</p>
+      </SignedOut>
+    </>
+  );
 }
